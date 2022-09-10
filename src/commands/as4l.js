c5251657466ba03af3f05c6b807305b1d1c32841
LawrenceLLC <giorgikobkhidze@gmail.com>
2022-09-10 17:44:24 +0400

    Create as4l.js
    Very secret easter egg

---
+++ src/commands/as4l.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'as4l',
+	helpTitle: 'as4l',
+	category: 'fun',
+	usage: 'as4l',
+	description: 'ALPHEN STREET 4 LIFE',
+	isDangerous: false,
+	mainServerOnly: false,
+	isHidden: false,
+    permissions: [],
+    devOnly: false,
+	run: async ({client, message, args}) => {
+		message.channel.send('Fo sho!');
+	},
+};
+
+module.exports.name = "as4l"
